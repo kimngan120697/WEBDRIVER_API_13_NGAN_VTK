a928ae1432ec39a9f449f811bcf8486638705a5b
ICARE <ICARE@KIMNGAN-20-10-2019>
2020-02-14 08:08:09 +0700

    topic 14 complete

--- selenium/webdriver_api/Topic_11_12_Popup_Iframe_Window.java
+++ selenium/webdriver_api/Topic_11_12_Popup_Iframe_Window.java
@@ -1,11 +1,14 @@
 package webdriver_api;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,20 +18,53 @@ import org.testng.annotations.Test;
 public class Topic_11_12_Popup_Iframe_Window {
 
 	WebDriver driver;
+	WebDriverWait waitExplicit;
 	
 	// Pre-condition
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
+		waitExplicit=new WebDriverWait(driver,10);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
 	}
 
 	@Test
-	public void TC_01_displayed() {
+	public void TC_01_displayed() throws InterruptedException {
+		driver.get("https://kyna.vn/");
 		
+		List <WebElement> fancyPopup=driver.findElements(By.xpath("//div[@class='fancybox-inner']"));
+		System.out.println("Fancybox popup is displayed: " + fancyPopup.size());
+		if (fancyPopup.size()>0)
+		{
+			Assert.assertTrue(fancyPopup.get(0).isDisplayed());
+			driver.findElement(By.cssSelector(".fancybox-close")).click();
+		}
 		
+		//Switch vào iframe trước thì mới tương tác với các element trong đó
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@src,\"facebook.com\")]")));
+		
+		boolean facebookIframe=driver.findElement(By.xpath("//a[text()='Kyna.vn']")).isDisplayed();
+		System.out.println("Facebook Iframe is displayed: " +facebookIframe);
+		Assert.assertTrue(facebookIframe);
+		
+		String likesFacebook=driver.findElement(By.xpath("//a[text()='Kyna.vn']/parent::div/following-sibling::div")).getText();
+		System.out.println("Facebook like number="+likesFacebook);
+		Assert.assertEquals(likesFacebook, "170K likes");
+		
+		//Switch về main page lại
+		driver.switchTo().defaultContent();
+		Thread.sleep(3000);
+		
+		driver.findElement(By.className("button-login")).click();
+		driver.findElement(By.id("user-login")).sendKeys("automationfc.vn@gmail.com");
+		Thread.sleep(3000);
+		driver.findElement(By.id("user-password")).sendKeys("automationfc.vn@gmail.com");
+		Thread.sleep(3000);
+		driver.findElement(By.id("btn-submit-login")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@class='user' and text()='Automation FC']")).isDisplayed());
 	}
 
 	@Test
